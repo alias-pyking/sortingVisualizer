937de709bfe2e3b1f47ed5a68bdc6c8eb578effa
shubhamsks <shubhamkumar7051@gmail.com>
2020-03-01 23:42:47 +0530

    added bubble sort animations

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ const showElements =(array)=>{
     }
 }
 
-const originalArray = [50,60,90,200,900,800,30,20,10,90,12,232,123,213,12,43,234,53,2,53,43,5,2,5,23,5,2,52,42,42,90,1000,394,292,948,23,342,89,232,1,22,122,12,113,902,232,47,47,88,88,27,32,45,78.98,78];
+const originalArray = [];
+let size = 200;
+for(let i = 1; i < size; i++) {
+    let number = Math.random() * 800;
+    originalArray.push(parseInt(number));
+}
 let array = [...originalArray];
-
+console.log(array);
 showElements(array);
 
 // Bubble sort algorithm visualization 
@@ -22,38 +27,49 @@ bubbleSortButton.addEventListener("click",(event) => {
     bubbleSort(bars);
 });
 
-function bubbleSortAnimate(bars,j) {
-    
+function bubbleSortAnimate(bars, order) {
+    document.getElementById(bars.item(order[0][0]).id).className = 'bar bar-selected';
+    document.getElementById(bars.item(order[0][1]).id).className = 'bar bar-selected'; 
+    for(let idx = 0; idx < order.length; idx++) {
+        let i = order[idx][0];
+        let j = order[idx][1];
+        setTimeout(() => {
+            let bar1 = Number.parseInt(bars.item(i).style.height.slice(0,- 2));
+            let bar2 = Number.parseInt(bars.item(j).style.height.slice(0,-2));
+            console.log(bar1, bar2);
+            console.log(idx);
+            if(bar1 > bar2) {
+                bars.item(i).style.height  = `${bar2}px`;
+                bars.item(j).style.height = `${bar1}px`;
+            }
+            document.getElementById(bars.item(i).id).className = 'bar';
+            document.getElementById(bars.item(j).id).className = 'bar bar-completed';
+            document.getElementById(bars.item(order[idx + 1][0]).id).className = 'bar bar-selected';
+            document.getElementById(bars.item(order[idx +1][1]).id).className = 'bar bar-selected';
+
+        },5 * idx); 
+    }
 }
 const bubbleSort = async(bars) => {
     // for(let i = 0; i < bars.length; i++) {
     //     bars.item(i).classList = [...bars.item(i).classList,'red'].join(' ');
     // }
+
     const n = bars.length;
+    const order = [];
     for(let i = 0; i < n - 1; i++) {
-        
-            setTimeout(function() {
-                for(let j = 0; j < n - i -1;j++) {
-                console.log(j);
-                document.getElementById(bars.item(j).id).className = 'bar bar-selected';
-                document.getElementById(bars.item(j + 1).id).className = 'bar bar-selected';
-                let bar1 = Number.parseInt(bars.item(j).style.height.slice(0,- 2));
-                let bar2 = Number.parseInt(bars.item(j + 1).style.height.slice(0,-2));
-                if(bar1 > bar2) {
-                    bars.item(j).style.height  = `${bar2}px`;
-                    bars.item(j + 1).style.height = `${bar1}px`;
-                }
-                document.getElementById(bars.item(j).id).className = 'bar';
-                document.getElementById(bars.item(j + 1).id).className = 'bar bar-completed'; 
-            }
-            },100 * (i+1));
+            for(let j = 0; j < n - i -1;j++) {
+                order.push([j,j+1]); 
         }
+    }
+    bubbleSortAnimate(bars, order);
+
 }
 const resetButton = document.getElementById('initial');
 
 resetButton.addEventListener("click",(event) => {
     const bars = document.getElementsByClassName("bar");
     for(let i = 0;i < bars.length; i++) {
-        bars.item(i).style.height = `${originalArray[i]}px`;
+        bars.item(i).style.height = `${array[i]}px`;
     }
 });
